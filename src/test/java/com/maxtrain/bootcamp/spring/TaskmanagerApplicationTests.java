ffcef2d80c607fe99a6598a2937dbc91801b82c3
David Benkner <d.benkner@gmail.com>
2023-09-22 12:19:55 -0400

    intial commit

---
+++ src/test/java/com/maxtrain/bootcamp/spring/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxtrain.bootcamp.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
